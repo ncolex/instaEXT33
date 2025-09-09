12ea813c97aebb0a49f4ea28359fb41711807097
ncolex <nicobattaglia33@gmail.com>
2025-09-09 05:45:35 -0300

    Add GitHub Pages deployment configuration

---
+++ instaEXT33/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/instaEXT33/' // Nombre exacto de tu repositorio
+})
